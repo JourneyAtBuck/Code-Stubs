f04a0f6b5b2ff0817350d559ebc7dd465c6f0380
Moses Journey <journey@buck.tv>
2017-10-04 14:11:47 -0700

    add aequery, color changer v1, other stuff

---
+++ AE/colorChanger.jsx
@@ -0,0 +1,16 @@
+﻿#target aftereffects
+(function () {
+    #include "./lib/aequery.js"
+    var oldColor = [0,0,0,1];
+    var newColor = [1,1,1,1];
+    
+    var oldColorString = oldColor.join("_");
+    var newColorString = newColor.join("_");
+    aeq.forEachProp(aeq.getActiveComp(), function(prop) {
+        if (prop.matchName == "ADBE Vector Fill Color" || prop.matchName == "ADBE Vector Stroke Color")
+            var valueString = prop.value.join("_");
+        if (valueString == oldColorString) {
+            prop.setValue(newColor);
+        }
+    })
+})()

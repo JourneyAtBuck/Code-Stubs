3671c0b7996ed7a0241121fa2e3a439c4b487d60
Moses Journey <journey@buck.tv>
2019-09-29 15:33:41 -0700

    shapeNameClearner - simplify, add error checking.

--- AE/ShapeNameCleaner.jsx
+++ AE/ShapeNameCleaner.jsx
@@ -1,16 +1,15 @@
-﻿{
-    try{
-        var comp = app.project.activeItem;
-        var selLyrs = comp.selectedLayers;
-        
-        //var newName = "Speech Bubble";
-        
-        app.beginUndoGroup("Rename selected layers");
-        for (i in selLyrs) {
-            selLyrs[i].name = selLyrs[i].name.substr(0,selLyrs[i].name.lastIndexOf(" "));
-        }
-        app.endUndoGroup();
-    } catch (e) {
-        alert(e);    
-    }
-}
+﻿(function cleanShapeLayerNames() {
+  var comp = app.project.activeItem;
+  if (!comp) return alert("No active comp.");
+  var selLyrs = comp.selectedLayers;
+  if (!selLyrs) return alert("No layers selected.");
+
+  app.beginUndoGroup("Rename selected layers");
+  for (i in selLyrs) {
+    selLyrs[i].name = selLyrs[i].name.substr(
+      0,
+      selLyrs[i].name.lastIndexOf(" ")
+    );
+  }
+  app.endUndoGroup();
+})();
